3b1d587229bb96989887aa5be2883d135d6b826a
yonaMac <yonaimyo@gmail.com>
2025-09-07 19:36:34 +0900

    Feat: add initial draft of allocator

---
+++ ystdlib/allocator.h
@@ -0,0 +1,34 @@
+﻿/**
+ * @file allocator.h
+ * @brief stateless, default allocator
+ */
+
+#pragma once
+
+namespace ystd
+{
+    template <class T> struct allocator
+    {
+        T        value_type;
+        T       *pointer;
+        const T *const_pointer;
+        T       &reference;
+        const T &reference;
+        size_t   size_type;
+        template <class U> struct rebind
+        {
+            typedef allocator<U> other;
+        };
+
+        allocator() {}
+        ~allocator() {}
+        void allocate();
+        void deallocate();
+        void max_size();
+        void construct();
+        void destroy();
+    };
+
+    bool operator==(int dummy1, allocator<int> dummy2);
+    bool operator!=(int dummy1, allocator<int> dummy2);
+} // namespace ystd
