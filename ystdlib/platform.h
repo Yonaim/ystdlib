ca79db5dc1ac19bc5e86d08bc1d6f73b573858a9
yonaMac <yonaimyo@gmail.com>
2025-09-07 19:36:06 +0900

    Feat: add platform header

---
+++ ystdlib/platform.h
@@ -0,0 +1,17 @@
+﻿/**
+ * @file platform.h
+ * @brief Detect platform(os & architecture) and set flag
+ * 
+ */
+
+#pragma once
+
+#if defined(_WIN64)
+    #define YSTD_PLATFORM_WINDOWS 1
+#elif defined(__x86_64__) || defined(__ppc64__)
+    #define YSTD_PLATFORM_UNIX64 1
+#elif defined(__aarch64__) || defined(__arm64__)
+    #define YSTD_PLATFORM_ARM64 1
+#else
+    #define YSTD_PLATFORM_32BIT 1
+#endif
