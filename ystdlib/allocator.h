583247d314463d0a7185dbc19e0e7382ba30e81b
yonaMac <yonaimyo@gmail.com>
2025-09-08 16:19:14 +0900

    Fix: interface of allocator

--- ystdlib/allocator.h
+++ ystdlib/allocator.h
@@ -15,8 +15,8 @@ namespace ystd
         using value_type = T;
         using pointer = T *;
         using const_pointer = const T *;
-        using reference = T&;
-        using const reference = const T&;
+        using reference = T &;
+        using const_reference = const T &;
         using size_type = size_t;
         using difference_type = ptrdiff_t;
 
@@ -25,15 +25,22 @@ namespace ystd
             typedef allocator<U> other;
         };
 
-        allocator() {}
-        ~allocator() {}
-        void allocate();
-        void deallocate();
-        void max_size();
-        void construct();
-        void destroy();
+        // member functions
+        allocator() noexcept;
+        allocator(const allocator &other) noexcept;
+        template <class U> allocator(const allocator<U> &other) noexcept;
+        ~allocator();
+        pointer       address(reference x) const noexcept;
+        const_pointer address(const_reference x) const noexcept;
+        pointer       allocate(size_type n, const void *hint = 0);
+        void          deallocate(T *p, std::size_t n);
+        size_type     max_size() const noexcept;
+        void          construct(pointer p, const_reference val);
+        void          destroy(pointer p);
     };
 
-    bool operator==(int dummy1, allocator<int> dummy2);
-    bool operator!=(int dummy1, allocator<int> dummy2);
+    template <class T1, class T2>
+    bool operator==(const allocator<T1> &lhs, const allocator<T2> &rhs) noexcept;
+    template <class T1, class T2>
+    bool operator!=(const allocator<T1> &lhs, const allocator<T2> &rhs) noexcept;
 } // namespace ystd
